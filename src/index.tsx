9e5232e48cfae163ce6af5ae93867ff9501c0ff0
Joshua Rodriguez <joshua.david.rodriguez.medrano@gmail.com>
2022-04-30 14:36:08 -0600

    Fix lint errors and node version

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import {useMediaQuery} from "react-responsive";
+import * as React from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 interface BreakpointsProps {
   sm: number;
@@ -12,37 +12,44 @@ const defaultBreakpoints = {
   sm: 320,
   md: 481,
   lg: 769,
-  xl: 1025
-}
+  xl: 1025,
+};
 
-interface ResponsiveProps<T extends React.FunctionComponent>{
+interface ResponsiveProps<T extends React.FunctionComponent> {
   sm?: Partial<React.ComponentProps<T>>;
   md?: Partial<React.ComponentProps<T>>;
   lg?: Partial<React.ComponentProps<T>>;
   xl?: Partial<React.ComponentProps<T>>;
 }
 
-export const useResponsive = (breakpoints: BreakpointsProps = defaultBreakpoints) => {
-  const {sm, md, lg, xl} = breakpoints;
+export const useResponsive = (
+  breakpoints: BreakpointsProps = defaultBreakpoints
+) => {
+  const { sm, md, lg, xl } = breakpoints;
 
-  return <T extends React.FunctionComponent>(Component: React.FunctionComponent, values: ResponsiveProps<T>) => {
+  const min = useMediaQuery({ maxWidth: sm - 1 });
+  const small = useMediaQuery({ minWidth: sm, maxWidth: md - 1 });
+  const medium = useMediaQuery({ minWidth: md, maxWidth: lg - 1 });
+  const large = useMediaQuery({ minWidth: lg, maxWidth: xl - 1 });
+  const extralarge = useMediaQuery({ minWidth: xl });
+
+  return <T extends React.FunctionComponent>(
+    Component: React.FunctionComponent,
+    values: ResponsiveProps<T>
+  ) => {
     const values_sm = values?.sm ?? {};
     const values_md = values?.md ?? values_sm;
     const values_lg = values?.lg ?? values_md;
     const values_xl = values?.xl ?? values_lg;
 
-    const min = useMediaQuery({maxWidth: sm - 1});
-    const small = useMediaQuery({minWidth: sm, maxWidth: md - 1});
-    const medium = useMediaQuery({minWidth: md, maxWidth: lg - 1});
-    const large = useMediaQuery({minWidth: lg, maxWidth: xl - 1});
-    const extralarge = useMediaQuery({minWidth: xl});
-
-    return (rest: React.ComponentProps<T>) => (<>
-      {min && <Component {...rest} />}
-      {small && <Component {...rest} {...values_sm} />}
-      {medium && <Component {...rest} {...values_md} />}
-      {large && <Component {...rest} {...values_lg} />}
-      {extralarge && <Component {...rest} {...values_xl} />}
-    </>)
+    return (rest: React.ComponentProps<T>) => (
+      <>
+        {min && <Component {...rest} />}
+        {small && <Component {...rest} {...values_sm} />}
+        {medium && <Component {...rest} {...values_md} />}
+        {large && <Component {...rest} {...values_lg} />}
+        {extralarge && <Component {...rest} {...values_xl} />}
+      </>
+    );
   };
-}
+};
